c0bf93645de7ae8217b9fa3d01a8f7d0599d0db7
Nipun753 <71968869+Nipun753@users.noreply.github.com>
2022-05-29 11:38:53 +0530

    Add files via upload

---
+++ React Test/29-may/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import ImageSlider from './Components/ImageSlider';
+import { SliderData } from './Components/SliderData';
+
+function App() {
+  return (
+    <ImageSlider slides={SliderData} />
+  );
+}
+
+export default App;
